250e2fec570e84b52f78ff217efe726cb65255e4
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2023-06-13 22:48:39 +0300

    style: make consistent indentations

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,12 +3,12 @@ import { Configuration } from 'webpack';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 
 const config: Configuration = buildWebpackConfig({
-  mode: 'development',
-  paths: {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: path.resolve(__dirname, 'dist'),
-    htmlTemplate: path.resolve(__dirname, 'public', 'index.html'),
-  }
+	mode: 'development',
+	paths: {
+		entry: path.resolve(__dirname, 'src', 'index.ts'),
+		output: path.resolve(__dirname, 'dist'),
+		htmlTemplate: path.resolve(__dirname, 'public', 'index.html'),
+	}
 });
 
 export default config;
