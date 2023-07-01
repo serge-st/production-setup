1a3f7c9128e287a55eb56efd3cefae2d9dcbd48a
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2023-07-01 15:47:05 +0300

    feat: testing css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
 import Counter from './Components/Counter';
 import './index.scss';
+import classes from './App.module.scss';
 
 const App: FC = () => {
     return (
         <div className='App'>
             <Counter />
+            <button className={classes.btn}>test</button>
+            <button className={classes.btn}>test red</button>
+            <p className="test">test p should be lime</p>
         </div>
     );
 };
