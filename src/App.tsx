8c40bc4dbcb6a1748db604482135972e3371e9a6
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2023-07-01 17:05:39 +0300

    feat: testing routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import { FC } from 'react';
 import Counter from './Components/Counter';
 import './index.scss';
 import classes from './App.module.scss';
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import { Link } from 'react-router-dom';
 
 const App: FC = () => {
     return (
         <div className='App'>
+            <Link to={'/'}>Main Page</Link>
+            <Link to={'/about'}>About Page</Link>
+            <Routes>
+                <Route path='/' element={<MainPage />} />
+                <Route path='/about' element={<AboutPage />} />
+            </Routes>
             <Counter />
             <button className={classes.btn}>test</button>
             <button className={classes.btn}>test red</button>
