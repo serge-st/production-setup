ca970d0bf6215d2d3cc360e836b287131d473193
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2023-08-16 19:25:26 +0300

    feat: add react lazy loading for pages

--- src/pages/AboutPage/AboutPage.async.tsx
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
 export const AboutPageAsync = lazy(() => import('./AboutPage'));
-
-// export const AboutPageAsync = lazy(() => {
-//     return setTimeout(() =>  import('./AboutPage'),1500);
-// });
