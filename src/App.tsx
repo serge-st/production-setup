9faa6e2480042dc1938b1c443a0075205a631017
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2023-08-20 11:49:56 +0300

    feat: implement theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useState } from 'react';
 import './styles/index.scss';
 import { Routes, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPageAsync';
 import { MainPageAsync } from './pages/MainPage/MainPageAsync';
 
+const THEME = {
+	REGULAR: 'regular',
+	DARK: 'dark',
+} as const;
+
+type ObjectValues<T> = T[keyof T];
+type Theme = ObjectValues<typeof THEME>;
+
 const App: FC = () => {
+    const [theme, setTheme] = useState<Theme>('regular');
+
+    const handleClick = () => {
+        setTheme(theme === 'regular' ? 'dark' : 'regular');
+    }
+
     return (
-        <div className='App regular'>
+        <div className={`App ${theme}`}>
+            <button type="button" onClick={handleClick}>Toggle Theme</button><br></br>
             <Link to={'/'}>Main Page</Link>
             <Link to={'/about'}>About Page</Link>
             
