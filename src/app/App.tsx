366d94a03bf0d719c39c36a686c3fe594639ddc9
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2023-09-01 18:57:36 +0300

    feat(i18n): add first translation example

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import { AppRouter } from 'app/providers/AppRouter';
 import { classNames, useTheme } from 'shared/lib';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar/UI/Sidebar/Sidebar';
+import { useTranslation } from 'react-i18next';
 
 export const App: FC = () => {
     const {theme} = useTheme();
@@ -11,8 +12,25 @@ export const App: FC = () => {
     // !! JUST FOR TESTING PURPOSES
     console.log(Math.random(), 'App render')
 
+    const { t, i18n } = useTranslation('pages/about');
+
+    const toggleLanguage = () => {
+        const lng = i18n.language === 'en' ? 'ru' : 'en';
+      i18n.changeLanguage(lng);
+    };
+
+    function MyComponent() {
+        return (
+          <div>
+            <button onClick={() => toggleLanguage()}>Change</button>
+            {t('title')}
+          </div>
+        );
+    }
+
     return (
         <div className={classNames('App', {}, [theme])}>
+            <MyComponent />
             <Navbar />
             <div className='main-content-wrapper'>
                 <Sidebar />
