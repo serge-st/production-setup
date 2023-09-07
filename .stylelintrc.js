d56cdc007ae3d72b84ae6e1dbcdce56ca0915b3f
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2023-09-07 20:52:35 +0300

    build(stylelin): add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'extends': 'stylelint-config-standard-scss',
+    'rules': {
+        'selector-class-pattern': null,
+        'property-no-vendor-prefix': null,
+    }
+}
+    
