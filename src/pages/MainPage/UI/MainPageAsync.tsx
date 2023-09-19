cf5c1a3259094b28deef51367d744ccfddaf7912
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2023-09-19 14:10:33 +0300

    feat(mobile UI): improve UI on mobile

--- src/pages/MainPage/UI/MainPageAsync.tsx
+++ src/pages/MainPage/UI/MainPageAsync.tsx
@@ -4,7 +4,7 @@ import { lazy } from 'react';
 export const MainPageAsync = lazy(() => {
     return new Promise(res => {
         setTimeout(() => {
-            // @ts-expect-error: for testing purposes
+            // @ts-expect-error: for testing purposes TODO: remove this later
             return res(import('./MainPage'));
         }, 1000);
     });
