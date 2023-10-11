e7f2971be12d679b5896c856619c0810886d9ded
Serge Stecenko <sergejs.stecenko@gmail.com>
2023-10-11 21:26:44 +0300

    fix(i18n): preserve selected launguage on refresh (add language detector)

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,8 +1,10 @@
 import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
+    .use(LanguageDetector)
     .use(Backend)
     .use(initReactI18next)
     .init({
