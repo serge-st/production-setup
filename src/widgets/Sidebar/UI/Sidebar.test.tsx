172a1e502719a4c9749800baa3414f13132868f3
Serge Stecenko <sergejs.stecenko@gmail.com>
2023-10-12 12:35:38 +0300

    test(Sidebar): improve test coverage

--- src/widgets/Sidebar/UI/Sidebar.test.tsx
+++ src/widgets/Sidebar/UI/Sidebar.test.tsx
@@ -1,10 +1,25 @@
-import { screen } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import { renderWithTranslation } from 'shared/lib/renderWithTranslation';
 import { Sidebar } from 'widgets/Sidebar/UI/Sidebar';
 
 describe('Test Sidebar widget component', () => {
     test('Basic render', () => {
-        renderWithTranslation(<Sidebar />);
-        expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+        const { container } = renderWithTranslation(<Sidebar />);
+        const [sidebar] = container.getElementsByClassName('Sidebar');
+        expect(sidebar).toBeInTheDocument();
+    });
+
+    test('To be collapsed on first render', () => {
+        const { container } = renderWithTranslation(<Sidebar />);
+        const [sidebar] = container.getElementsByClassName('Sidebar');
+        expect(sidebar).toHaveClass('collapsed');
+    });
+
+    test('Sidebar oppening (button toggle)', () => {
+        const { container } = renderWithTranslation(<Sidebar />);
+        const [sidebar] = container.getElementsByClassName('Sidebar');
+        const [toggleButton] = container.getElementsByClassName('arrow-button');
+        fireEvent.click(toggleButton);
+        expect(sidebar).not.toHaveClass('collapsed');
     });
 });
