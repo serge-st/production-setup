79283b91ab9d460ecb2812c0492dc9838c964f3f
Serge Stecenko <sergejs.stecenko@gmail.com>
2023-10-12 10:22:43 +0300

    test(AppButton): add additional test cases

--- src/shared/UI/AppButton/AppButton.test.tsx
+++ src/shared/UI/AppButton/AppButton.test.tsx
@@ -6,4 +6,19 @@ describe('Test AppButton UI component', () => {
         render(<AppButton>TEST</AppButton>);
         expect(screen.getByText('TEST')).toBeInTheDocument();
     });
+
+    test('Default class is applied', () => {
+        render(<AppButton>TEST</AppButton>);
+        expect(screen.getByText('TEST')).toHaveClass('clear');
+    });
+
+    test('Inversed theme class is applied', () => {
+        render(<AppButton theme='clear-inversed'>TEST</AppButton>);
+        expect(screen.getByText('TEST')).toHaveClass('clear-inversed');
+    });
+
+    test('Component attributes are applied', () => {
+        render(<AppButton type='submit'>TEST</AppButton>);
+        expect(screen.getByText('TEST')).toHaveAttribute('type', 'submit');
+    });
 });
