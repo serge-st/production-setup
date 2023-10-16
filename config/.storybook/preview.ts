4c40349751b45c1034964873718e7ec47d2e098e
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2023-10-16 16:06:17 +0300

    feat(Navbar.stories): add Navbar stories, add storybook router support

--- config/.storybook/preview.ts
+++ config/.storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from "@storybook/react";
 import { ThemeDecorator } from '../../src/shared/config/storybook/decorators/ThemeDecorator';
 import { getThemeType } from './globals/getThemeType';
 import { getLocaleType } from './globals/getLocaleType';
+import { withRouter } from 'storybook-addon-react-router-v6';
 
 const preview: Preview = {
   parameters: {
@@ -18,6 +19,7 @@ const preview: Preview = {
     locale: getLocaleType(),
   },
   decorators: [
+    withRouter,
     ThemeDecorator(),
   ],
 };
