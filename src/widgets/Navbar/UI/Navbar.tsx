908c9d9d23da0c82aeb6bc9bd6f09281dc96f13c
Serge Stecenko <sergejs.stecenko@gmail.com>
2023-10-18 20:08:20 +0300

    feat(Navbar): display active route

--- src/widgets/Navbar/UI/Navbar.tsx
+++ src/widgets/Navbar/UI/Navbar.tsx
@@ -1,8 +1,10 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { classNames } from 'shared/lib';
 import cls from './Navbar.module.scss';
 import { AppLink } from 'shared/UI';
 import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
+import { routeConfig } from 'shared/config/routeConfig';
 
 interface NavbarProps {
     className?: string;
@@ -10,12 +12,36 @@ interface NavbarProps {
 
 export const Navbar: FC<NavbarProps> = ({className}) => {
     const { t } = useTranslation();
+    let location = useLocation();
+    const [activePath, setActivePath] = useState<string | null>(null)
+    const appRoutes = routeConfig.filter(({path}) => path !== '*')
+
+    const handleClick = (path: string): void => {
+        setActivePath(path)
+        console.log(location)
+    }
+
+    useEffect(() => {
+        setActivePath(location.pathname);
+    }, [activePath])
 
     return (
         <nav className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                <AppLink to={'/'}>{t('Nav main page name')}</AppLink>
-                <AppLink to={'/about'}>{t('Nav about page name')}</AppLink>
+                {appRoutes.map(({path}) => {
+                    const name = `Nav route name ${path}`
+
+                    return (
+                        <AppLink 
+                            key={path} 
+                            to={path} 
+                            onClick={() => handleClick(path)}
+                            theme={activePath === path ? 'clear-inversed-underlined' : 'clear-inversed'}
+                        >
+                            {t(name)}
+                        </AppLink>
+                    )
+                })}
             </div>
         </nav>
     );
