094a4661e95b5164e48b8d0c4ce3f0d3700210c4
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-01-11 18:57:51 +0200

    feat(AppInput): add basic styles

--- src/shared/UI/AppInput/AppInput.tsx
+++ src/shared/UI/AppInput/AppInput.tsx
@@ -2,13 +2,21 @@ import { FC, InputHTMLAttributes } from 'react';
 import { classNames } from 'shared/lib';
 import cls from './AppInput.module.scss';
 
+const APP_INPUT_THEME = {
+    CLEAR: 'clear',
+    CLEAR_INVERSED: 'clear-inversed',
+} as const;
+
+type AppInputTheme = ObjectValues<typeof APP_INPUT_THEME>;
+
 interface AppInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
     className?: string;
     onChange?: (value: string) => void;
+    theme?: AppInputTheme;
 }
 
 export const AppInput: FC<AppInputProps> = (props) => {
-    const { onChange, className, type = 'text', ...otherProps } = props;
+    const { onChange, className, type = 'text', theme = 'clear', ...otherProps } = props;
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         onChange?.(e.target.value);
@@ -16,7 +24,7 @@ export const AppInput: FC<AppInputProps> = (props) => {
 
     return ( 
         <input 
-            className={classNames(cls.AppInput, {}, [className])}
+            className={classNames(cls.AppInput, {}, [className, cls[theme]])}
             type={type}
             {...otherProps}
             onChange={handleChange}
