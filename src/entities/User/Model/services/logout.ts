cf176392ce136154fd422cf391283d8ae7a26682
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-02-14 20:41:10 +0200

    refactor(User): code cleanup

--- src/entities/User/Model/services/logout.ts
+++ src/entities/User/Model/services/logout.ts
@@ -18,26 +18,3 @@ export const logout = createAsyncThunk<void, undefined, { rejectValue: string }>
         }
     },
 );
-
-// export const loginByUsername = createAsyncThunk<UserSchema, undefined, { rejectValue: LoginByUsernameError }>(
-//     'login/loginByUsername',
-//     async (_arg, thunkAPI) => {
-//         const { getState, rejectWithValue, dispatch } = thunkAPI;
-//         const { login } = getState() as StateSchema;
-//         const { username, password } = login;
-
-//         try {
-//             const response = await axios.post<UserSchema>('http://localhost:3335/auth/login', { username, password });
-
-//             if (!response.data) throw new Error('Unexpected response');
-
-//             localStorage.setItem(USER_ACCESS_TOKEN, JSON.stringify(response.data));
-//             dispatch(userActions.setUserData(response.data));
-//             return response.data;
-//         } catch (error) {
-//             if (!isAxiosError(error)) return rejectWithValue(error.message);
-//             if (Array.isArray(error.response.data.message)) return rejectWithValue(error.response.data.message.join('; '));
-//             return rejectWithValue(error.response.data.message);
-//         }
-//     },
-// );
